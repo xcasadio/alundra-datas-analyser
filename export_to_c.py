dde96f30c474f77f7d24ce12bc8409bab18ae62a
xcasadio <casaprod@hotmail.fr>
2025-04-27 11:17:07 +0200

    update script

--- export_to_c.py
+++ export_to_c.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#Export functions and static variables 
 #@author Xavier Casadio
 #@category Export
 #@keybinding
@@ -9,46 +10,20 @@ from java.io import FileWriter
 from ghidra.app.decompiler import DecompInterface
 from ghidra.util.task import ConsoleTaskMonitor
 from ghidra.program.model.symbol import SymbolType
-from ghidra.program.model.data import Array
-import os, struct
+from ghidra.program.model.data import Pointer, StringDataType, Array
+import os
 
 # === Setup output ===
 base_dir = os.path.join(os.getcwd(), "export_project")
 if not os.path.exists(base_dir):
     os.makedirs(base_dir)
 
-f_globals_h = FileWriter(os.path.join(base_dir, "globals.h"))
-f_globals_c = FileWriter(os.path.join(base_dir, "globals.c"))
-f_functions  = FileWriter(os.path.join(base_dir, "functions.c"))
+f_globals = FileWriter(os.path.join(base_dir, "globals.h"))
+f_functions = FileWriter(os.path.join(base_dir, "functions.c"))
 
-# === Export globals (globals.h) ===
-memory = currentProgram.getMemory()
+# === Export globals (globals.h), excluding type "string" and all pointers ===
 symbolTable = currentProgram.getSymbolTable()
 symbols = symbolTable.getAllSymbols(True)
-
-def read_value_bytes(addr, size):
-    bytes = bytearray(size)
-    try:
-        memory.getBytes(addr, bytes)
-        return bytes
-    except:
-        return None
-
-def format_value(bytes, datatype):
-    if not bytes:
-        return None
-    try:
-        if datatype.lower() in ["int", "undefined4"]:
-            return str(struct.unpack("<i", bytes[:4])[0])
-        elif datatype.lower() in ["short", "undefined2"]:
-            return str(struct.unpack("<h", bytes[:2])[0])
-        elif datatype.lower() in ["char", "undefined1"]:
-            return str(struct.unpack("<b", bytes[:1])[0])
-        elif datatype.lower() == "float":
-            return str(struct.unpack("<f", bytes[:4])[0])
-    except:
-        return None
-    return None
 
 for sym in symbols:
     if sym.getSymbolType() in [SymbolType.GLOBAL, SymbolType.LABEL]:
@@ -57,13 +32,12 @@ for sym in symbols:
         if var_name.lower().startswith("switchdata") or var_name.startswith("PTR_case"):
             continue
 
-        data = getDataAt(sym.getAddress())
-        if not data:
-            continue
-
         try:
+            data = getDataAt(sym.getAddress())
+            if data is None:
+                continue
+
             data_type = data.getDataType()
-            type_name = data_type.getDisplayName()
             address = sym.getAddress().getOffset()
 
             # base type
@@ -71,44 +45,28 @@ for sym in symbols:
                 base_type = data_type.getBaseDataType()
                 base_type_name = base_type.getName().lower()
             except:
+                base_type = data_type
                 base_type_name = data_type.getName().lower()
 
             if base_type_name in ["string", "pointer"]:
                 continue
 
             if isinstance(data_type, Array):
-                elem_type = data_type.getDataType().getDisplayName()
+                element_type = data_type.getDataType().getDisplayName()
                 count = data_type.getNumElements()
-                values = []
-
-                for i in range(count):
-                    try:
-                        comp = data.getComponent(i)
-                        val = comp.getValue()
-                        if isinstance(val, (int, long, float)):
-                            values.append(str(val))
-                        else:
-                            values.append("0")
-                    except:
-                        values.append("0")
-
-                f_globals_c.write("{} {}[{}] = {{{}}}; // 0x{:08X}\n".format(
-                    elem_type, var_name, count, ", ".join(values), address))
-
+                f_globals.write("extern {} {}[{}]; // {:08X}\n".format(
+                    element_type, var_name, count, address))
             else:
-                value = data.getValue()
-                if isinstance(value, (int, long, float)):
-                    f_globals_c.write("{} {} = {}; // {:08X}\n".format(type_name, var_name, value, address))
-                else:
-                    f_globals_c.write("{} {}; // {:08X}\n".format(type_name, var_name, address))
+                type_name = data_type.getDisplayName()
+                f_globals.write("extern {} {}; // {:08X}\n".format(
+                    type_name, var_name, address))
 
         except Exception as e:
-            print("Erreur sur", var_name, ":", str(e))
+            print("Error", var_name, ":", str(e))
             pass
 
 
-f_globals_h.close()
-f_globals_c.close()
+f_globals.close()
 
 # === Export functions (functions.c) ===
 f_functions.write("#include \"types.h\"\n")
@@ -121,18 +79,36 @@ decomp.openProgram(currentProgram)
 functionManager = currentProgram.getFunctionManager()
 functions = functionManager.getFunctions(True)
 
+prefixes_to_exclude = [
+     "_", "BIOS_", "Cd", "CD_", "EVENT_", "EXT_", "FONT_", "GPU_", "INTR_", "ISO9660_", 
+    "Krom2RawAdd", "MEM", "PAD", "PRNT_", "PUT", "S_", "SPRINTF_", 
+    "SPU_", "SR_", "STR", "SYS_", "Spu", "Ss", "VSYNC", "gte_", "v_", "get_", "def_",
+    "ClearImage", "LoadImage", "StoreImage", "MoveImage", "SetDrawArea",
+    "GetODE", "SetTexWindow", "SetDrawArea", "SetDrawOffset", "SetPriority", "SetDrawMode"
+]
+
+contains_to_exclude = ["Intr", "DispEnv", "DrawEnv", "OTag", "Sync", "Graph"]
+
 for func in functions:
     try:
-        if func.isExternal() or func.getBody().isEmpty():
+        func_name = func.getName()
+
+        if any(func_name.startswith(prefix) for prefix in prefixes_to_exclude):
+            print("Exclude function (1): {}".format(func_name))
             continue
-        if func.getName().startswith("gte_"):
+
+        if any(word in func_name for word in contains_to_exclude):
+            print("Exclude function (2): {}".format(func_name))
+            continue
+
+        if func.isExternal() or func.getBody().isEmpty():
             continue
 
         res = decomp.decompileFunction(func, 60, monitor)
         if res and res.getDecompiledFunction():
             code = res.getDecompiledFunction().getC()
             func_addr = func.getEntryPoint().getOffset()
-            f_functions.write("// {:08X}".format(func_addr))
+            f_functions.write("//{:08X}".format(func_addr))
             f_functions.write(code)
 
     except Exception as e:
